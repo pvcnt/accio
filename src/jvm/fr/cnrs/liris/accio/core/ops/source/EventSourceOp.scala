bcabdb7a282f05787160976dab17fba40aa5162b
Vincent <vincent.primault@gmail.com>
2016-06-22 13:25:50 +0000

    Support different kinds of event sources.
    GitOrigin-RevId: 9dc767a07b51e4b644d09090eb2fb985f1ebee3b

--- src/jvm/fr/cnrs/liris/accio/core/ops/source/EventSourceOp.scala
+++ src/jvm/fr/cnrs/liris/accio/core/ops/source/EventSourceOp.scala
@@ -2,7 +2,7 @@ package fr.cnrs.liris.accio.core.ops.source
 
 import fr.cnrs.liris.accio.core.dataset.{Dataset, DatasetEnv}
 import fr.cnrs.liris.accio.core.framework.{Op, Source}
-import fr.cnrs.liris.accio.core.io.CsvSource
+import fr.cnrs.liris.accio.core.io.{CabspottingSource, CsvSource, GeolifeSource}
 import fr.cnrs.liris.accio.core.model.Trace
 import fr.cnrs.liris.accio.core.param.Param
 import fr.cnrs.liris.common.util.FileUtils
@@ -15,13 +15,21 @@ import fr.cnrs.liris.common.util.FileUtils
 case class EventSourceOp(
     @Param(help = "Dataset URL")
     url: String,
+    @Param(help = "Kind of dataset")
+    kind: String = "csv",
     @Param(help = "Sampling ratio")
     sample: Option[Double],
     @Param(help = "User identifiers")
     users: Seq[String]
 ) extends Source {
+
   override def get(env: DatasetEnv): Dataset[Trace] = {
-    val source = CsvSource(FileUtils.replaceHome(url))
+    val source = kind match {
+      case "csv" => CsvSource(FileUtils.replaceHome(url))
+      case "cabspotting" => CabspottingSource(FileUtils.replaceHome(url))
+      case "geolife" => GeolifeSource(FileUtils.replaceHome(url))
+      case _ => throw new IllegalArgumentException(s"Unknown kind: $kind")
+    }
     var data = env.read(source)
     if (sample.nonEmpty) {
       data = data.sample(sample.get)
