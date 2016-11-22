dac1caf4117085c1752934e961241d05a30bac3e
Vincent <vincent.primault@gmail.com>
2016-11-22 10:38:38 +0000

    Directory names in reports are now without slashes.
    GitOrigin-RevId: 1117be227db877424aab3e24526733f33bff23dd

--- src/jvm/fr/cnrs/liris/accio/core/reporting/CsvReportCreator.scala
+++ src/jvm/fr/cnrs/liris/accio/core/reporting/CsvReportCreator.scala
@@ -49,7 +49,14 @@ class CsvReportCreator {
     if (opts.split) {
       // When splitting, one sub-directory is created per combination of workflow parameters.
       artifacts.split.foreach { list =>
-        val label = list.params.map { case (k, v) => s"$k=$v" }.mkString(",")
+        val label = list.params.map { case (k, v) =>
+          var vStr = v.toString
+          if (vStr.contains('/')) {
+            // Remove any slash that would be polluting directory name.
+            vStr = vStr.substring(vStr.lastIndexOf('/') + 1)
+          }
+          s"$k=$vStr"
+        }.mkString(",")
         doWrite(list, workDir.resolve(label), opts)
       }
     } else {
