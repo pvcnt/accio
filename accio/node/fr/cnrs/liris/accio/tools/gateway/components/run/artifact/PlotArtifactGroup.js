b46f674906f7422983087e80ef21fb90da168fba
Vincent Primault <vincent.primault@gmail.com>
2018-04-03 21:26:26 +0100

    Restore plots display

--- accio/node/fr/cnrs/liris/accio/tools/gateway/components/run/artifact/PlotArtifactGroup.js
+++ accio/node/fr/cnrs/liris/accio/tools/gateway/components/run/artifact/PlotArtifactGroup.js
@@ -16,105 +16,106 @@
  * along with Accio.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React from 'react'
-import {map, noop, concat, without, values, min, max, range} from 'lodash'
-import {Row, Col, ButtonGroup, Button, Glyphicon} from 'react-bootstrap'
-//import Plot from './Plot'
-import autobind from 'autobind-decorator'
+import React from 'react';
+import {concat, map, max, min, noop, range, values, without} from 'lodash';
+import {Button, ButtonGroup, Col, Glyphicon, Row} from 'react-bootstrap';
+import autobind from 'autobind-decorator';
 
-const STEPS = 100
+import Plot from './Plot';
+
+const STEPS = 100;
 
 function _count(points, from, to) {
-  let c = 0
+  let c = 0;
   points.forEach(v => {
     if (v > from && v <= to) {
-      c++
+      c++;
     }
-  })
-  return c
+  });
+  return c;
 }
 
 function _cdf(points, steps) {
-  if (points.length == 0) {
-    return []
+  if (points.length === 0) {
+    return [];
   }
-  const minValue = min(points)
-  const maxValue = max(points)
-  if (minValue == maxValue) {
-    return [{x: minValue, y: 1}]
+  const minValue = min(points);
+  const maxValue = max(points);
+  if (minValue === maxValue) {
+    return [{x: minValue, y: 1}];
   }
-  const by = (maxValue - minValue) / steps
-  const n = points.length
-  let x = [minValue]
-  let y = [_count(points, minValue - 1, minValue) / n]
+  const by = (maxValue - minValue) / steps;
+  const n = points.length;
+  let x = [minValue];
+  let y = [_count(points, minValue - 1, minValue) / n];
   range(minValue, maxValue, by).forEach(to => {
-    x.push(to)
-    y.push(_count(points, minValue, to) / n)
-  })
-  x.push(maxValue)
-  y.push(1)
-  return {x, y}
+    x.push(to);
+    y.push(_count(points, minValue, to) / n);
+  });
+  x.push(maxValue);
+  y.push(1);
+  return {x, y};
 }
 
 function _pdf(points, steps) {
-  if (points.length == 0) {
-    return []
+  if (points.length === 0) {
+    return [];
   }
-  const minValue = min(points)
-  const maxValue = max(points)
-  if (minValue == maxValue) {
-    return [{x: minValue, y: points.length}]
+  const minValue = min(points);
+  const maxValue = max(points);
+  if (minValue === maxValue) {
+    return [{x: minValue, y: points.length}];
   }
-  const by = (maxValue - minValue) / steps
-  let x = [minValue]
-  let y = [_count(points, minValue - 1, minValue)]
+  const by = (maxValue - minValue) / steps;
+  let x = [minValue];
+  let y = [_count(points, minValue - 1, minValue)];
   range(minValue, maxValue, by).forEach(to => {
-    x.push(to)
-    y.push(_count(points, to - by, to))
-  })
-  x.push(maxValue)
-  y.push(_count(points, maxValue - by, maxValue))
-  return {x, y}
+    x.push(to);
+    y.push(_count(points, to - by, to));
+  });
+  x.push(maxValue);
+  y.push(_count(points, maxValue - by, maxValue));
+  return {x, y};
 }
 
 class PlotArtifactGroup extends React.Component {
   constructor(props) {
-    super(props)
-    this.state = {type: 'pdf', selected: []}
+    super(props);
+    this.state = {type: 'pdf', selected: []};
   }
 
   @autobind
   _handleChange(type) {
-    this.setState({type})
+    this.setState({type});
   }
 
   @autobind
   _handleSelect(e, name) {
-    e.nativeEvent.preventDefault()
+    e.nativeEvent.preventDefault();
     if (this._isSelected(name)) {
-      this.setState({selected: without(this.state.selected, name)})
+      this.setState({selected: without(this.state.selected, name)});
     } else {
-      this.setState({selected: concat(this.state.selected, name)})
+      this.setState({selected: concat(this.state.selected, name)});
     }
   }
 
   @autobind
   _isSelected(name) {
-    return this.state.selected.indexOf(name) > -1
+    return this.state.selected.indexOf(name) > -1;
   }
 
   render() {
     //TODO
-    /*const cols = this.props.artifacts.map((artifact, idx) => {
-      const isSelected = this._isSelected(artifact.name)
-      const kind = (artifact.value.kind.base == 'map') ? artifact.value.kind.args[1] : artifact.value.kind.args[0]
-      const unit = (kind == 'distance') ? 'meters' : (kind == 'duration') ? 'milliseconds' : null
+    const cols = this.props.artifacts.map((artifact, idx) => {
+      const isSelected = this._isSelected(artifact.name);
+      const kind = (artifact.value.kind.base === 'map') ? artifact.value.kind.args[1] : artifact.value.kind.args[0];
+      const unit = (kind === 'distance') ? 'meters' : (kind === 'duration') ? 'milliseconds' : null;
 
-      let data = (artifact.value.kind.base == 'map') ? values(artifact.value.payload) : artifact.value.payload
-      if (this.state.type == 'cdf') {
-        data = _cdf(data, STEPS)
-      } else if (this.state.type == 'pdf') {
-        data = _pdf(data, STEPS)
+      let data = (artifact.value.kind.base === 'map') ? values(artifact.value.payload) : artifact.value.payload;
+      if (this.state.type === 'cdf') {
+        data = _cdf(data, STEPS);
+      } else if (this.state.type === 'pdf') {
+        data = _pdf(data, STEPS);
       }
 
       return (<Col md={isSelected ? 12 : 4} key={idx}>
@@ -124,8 +125,9 @@ class PlotArtifactGroup extends React.Component {
               type={this.state.type}
               width={(isSelected) ? 1110 : 350}
               height={(isSelected) ? 500 : 350}/>
-        <a href="#" onClick={e => this._handleSelect(e, artifact.name)} className="accio-summary-resize">
-          <Glyphicon glyph={(isSelected) ? 'resize-small' : 'resize-full'} />
+        <a href="#" onClick={e => this._handleSelect(e, artifact.name)}
+           className="accio-summary-resize">
+          <Glyphicon glyph={(isSelected) ? 'resize-small' : 'resize-full'}/>
         </a>
       </Col>);
     });
@@ -143,13 +145,12 @@ class PlotArtifactGroup extends React.Component {
         </div>
         <Row>{cols}</Row>
       </div>
-    );*/
-    return null;
+    );
   }
 }
 
 PlotArtifactGroup.propTypes = {
   artifacts: React.PropTypes.arrayOf(React.PropTypes.object).isRequired,
-}
+};
 
 export default PlotArtifactGroup;
